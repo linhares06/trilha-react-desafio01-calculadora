6a7285c15d846d2dcfc23f0f3a36cd333f3630be
unknown <felinhares06@gmail.com>
2024-12-13 12:38:23 -0300

    Commiting modifications to calculator

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Input from './components/Input';
 import Button from './components/Button';
 
@@ -7,92 +6,115 @@ import { useState } from 'react';
 
 
 const App = () => {
-  const [currentNumber, setCurrentNumber] = useState('0');
-  const [firstNumber, setFirstNumber] = useState('0');
-  const [operation, setOperation] = useState('');
+    const [currentNumber, setCurrentNumber] = useState('0');
+    const [firstNumber, setFirstNumber] = useState(null);
+    const [operation, setOperation] = useState('');
 
-  const handleOnClear = () => {
-    setCurrentNumber('0')
-    setFirstNumber('0')
-    setOperation('')
-  };
-
-  const handleAddNumber = (num) => {
-    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
-  }
-
-  const handleSumNumbers = () => {
-
-    if(firstNumber === '0'){
-        setFirstNumber(String(currentNumber));
+    const handleOnClear = () => {
         setCurrentNumber('0')
-        setOperation('+')
-    }else {
-      const sum = Number(firstNumber) + Number(currentNumber);
-      setCurrentNumber(String(sum))
-      setOperation('')
+        setFirstNumber(null)
+        setOperation('')
+    };
+
+    const handleAddNumber = (num) => {
+        setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
     }
 
-  }
-
-  const handleMinusNumbers = () => {
-
-    if(firstNumber === '0'){
-        setFirstNumber(String(currentNumber));
-        setCurrentNumber('0')
-        setOperation('-')
-    }else {
-      const sum = Number(firstNumber) - Number(currentNumber);
-      setCurrentNumber(String(sum))
-      setOperation('')
-    }
-
-  }
-
-  const handleEquals = () => {
-
-    if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
-        switch(operation){
-          case '+':
-            handleSumNumbers();
-            break;
-          case '-':
-            handleMinusNumbers();
-            break;
-          default: 
-            break;
+    const calculate = (operation, firstNumber, secondNumber) => {
+        const num1 = Number(firstNumber);
+        const num2 = Number(secondNumber);
+        console.log(num1, num2);
+        switch(operation) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+            default:
+                return secondNumber;
         }
     }
 
-  }
+    const handlePercentage = () => {
+        const percentage = Number(currentNumber) / 100;
+        if (!validateValidNumber(percentage)) return;
+        setCurrentNumber(String(percentage));
+    };
+
+    const handleSquareRoot = () => {
+        const sqrt = Math.sqrt(Number(currentNumber));
+        if (!validateValidNumber(sqrt)) return;
+        setCurrentNumber(String(sqrt));
+    };
+
+    const handleOperation = (op) => {
+        if (firstNumber === null) {
+            setFirstNumber(currentNumber);
+            setCurrentNumber('0');
+            setOperation(op);
+        } else if (operation) {
+            const result = calculate(operation, firstNumber, currentNumber);
+            if (!validateValidNumber(result)) return;
+            setFirstNumber(String(result));
+            setCurrentNumber('0');
+            setOperation(op);
+        }
+    };
+
+    const handleEquals = () => {
+        if (firstNumber !== null && operation && currentNumber !== '0') {
+            const result = calculate(operation, firstNumber, currentNumber);
+            if (!validateValidNumber(result)) return;
+            setCurrentNumber(String(result));
+            setFirstNumber(null);
+            setOperation('');
+        }
+    };
+
+    function validateValidNumber(number) {
+        if (number === Infinity || Number.isNaN(number)) {
+            setCurrentNumber('Error');
+            return false;
+        }
+        return true;
+    }
 
   return (
     <Container>
       <Content>
         <Input value={currentNumber}/>
         <Row>
-          <Button label="x"/>
-          <Button label="/"/>
-          <Button label="c" onClick={handleOnClear}/>
-          <Button label="."/>
+          <Button label="c" onClick={() => handleOnClear()}/>
+          <Button label="√" onClick={() => handleSquareRoot()}/>
+          <Button label="%" onClick={() => handlePercentage()}/>
+          <Button label="/" onClick={() => handleOperation('/')}/>
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={handleMinusNumbers}/>
+          <Button label="*" onClick={() => handleOperation('*')}/>
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="5" onClick={() => handleAddNumber('5')}/>
           <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={handleSumNumbers}/>
+          <Button label="-" onClick={() => handleOperation('-')}/>
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="=" onClick={handleEquals}/>
+          <Button label="+" onClick={() => handleOperation('+')}/>
+        </Row>
+        <Row>
+          <Button label="0" onClick={() => handleAddNumber('0')}/>
+          <Button label="."/>
+          <Button label="."/>
+          <Button label="=" onClick={() => handleEquals()}/>
         </Row>
       </Content>
     </Container>
